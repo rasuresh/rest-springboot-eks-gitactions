8f8d83b52cb1707d96b83c5820526b3ae3a92f27
SR <sure.ranga@gmail.com>
2025-08-05 22:11:55 -0500

    first commit

---
+++ src/main/java/com/suresh/api/restspringbooteksgitactions/RestSpringbootEksGitactionsApplication.java
@@ -0,0 +1,13 @@
+package com.suresh.api.restspringbooteksgitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootEksGitactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSpringbootEksGitactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suresh/api/restspringbooteksgitactions/RestSpringbootEksGitactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh.api.restspringbooteksgitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootEksGitactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
